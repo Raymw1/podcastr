f486ddb9358b60beeb58614a0dde3eaca07afbfd
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-25 21:32:16 -0300

    Generating static home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,3 +5,18 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+/* ------------- SSR ------------- */
+/* export async function getServerSideProps() { */
+
+/* ------------- SSG ------------- */
+export async function getStaticSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
