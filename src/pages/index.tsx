3c0922a7874a0b213ae2e34b7a3805cac7be71ee
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-26 15:42:32 -0300

    Serialize and type for request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import type { NextPage } from 'next';
+import { format, parseISO } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import type { GetStaticProps, NextPage } from 'next';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-const Home: NextPage = () => {
-  return <h1>Index</h1>;
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  /* episodes: Array<Episode>; */
+  episodes?: Episode[];
+};
+
+const Home: NextPage = (props: HomeProps) => {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
 };
 
 export default Home;
@@ -10,13 +30,30 @@ export default Home;
 /* export async function getServerSideProps() { */
 
 /* ------------- SSG ------------- */
-export async function getStaticSideProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+  const episodes = data.map((episode: any) => ({
+    ...episode,
+    publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+      locale: enUS,
+    }),
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(
+      Number(episode.file.duration)
+    ),
+    url: episode.file.url,
+  }));
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
